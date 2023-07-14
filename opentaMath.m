bc3972cdef23c6a94170877042b28ea43106b4e9
Stellan Ostlund <stellan.ostlund@gmail.com>
2023-07-15 00:51:19 +0200

    Improve error handling

--- opentaMath.m
+++ opentaMath.m
@@ -1,14 +1,20 @@
   opentaMath[ x_] :=
     Module[ {q,y,res,ret,success,messages},
-      q = ToExpression[ x ];
-      res = EvaluationData[  Expand[ q ]  ];
-      Print["RES = ", res ];
-      y = Lookup[res, "Result"];
-      success = Lookup[res,"Success"];
-      messages = Lookup[res,"Messages"];
-      ret = If[ success,  y ,  messages[[1]]  ];
-      Print[ ret ];
-      Print[ success ];
-      Return[ ret ]
+      res0 = EvaluationData[ ToExpression[ x ] ];
+      Print["RES0 = ", res0 ];
+      success = Lookup[res0,"Success"];
+      messages = Lookup[res0,"Messages"];
+      If[ success, 
+      	q = Lookup[res0,"Result"];
+      	res = EvaluationData[ Simplify[   q    ] ];
+      	Print["RES = ", res ];
+      	y = Lookup[res, "Result"];
+      	success = Lookup[res,"Success"];
+      	messages = Lookup[res,"Messages"];
+      	ret = If[ success,  y ,  messages[[1]]  ];
+      	Print[ ret ];
+      	Print[ success ];
+	, ret = messages  ];
+     Return[ ret ]
     ]
 
